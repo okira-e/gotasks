0a0c50db3c1c8fc8b3c4aeb446ca58e90bd96b86
Omar <omarrafat60@gmail.com>
2024-09-30 00:55:13 +0300

    Added list command

--- cmd/list-all-boards.go
+++ cmd/list-all-boards.go
@@ -1,6 +1,12 @@
 package cmd
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/okira-e/gotasks/internal/domain"
 	"github.com/spf13/cobra"
 )
 
@@ -9,6 +15,41 @@ var ListAllBoards = &cobra.Command{
 	Short: "List all the boards.",
 	Long:  `List all boards saved in the user config.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// @Todo: Make this list every board in the config including the directory of the board that it was created at.
+		config, err := domain.GetUserConfig()
+		if err != nil {
+			log.Fatalf("Failed to get the user config. %s", err)
+		}
+
+		t := table.NewWriter()
+		t.SetOutputMirror(os.Stdout)
+		t.AppendHeader(table.Row{"#", "Board", "Number of tasks", "Progress", "Path"})
+		
+		for i, board := range config.Boards {
+			totalNumberOfTasks := 0
+			numberOfCompletedTasks := 0
+			for i, columnName := range board.Columns {
+				totalNumberOfTasks += len(board.Tasks[columnName])
+				
+				if i == len(board.Columns) - 1 { // Check if we are on the last column to the right.
+					numberOfCompletedTasks += len(board.Tasks[columnName])					
+				}
+			}
+			
+			progress := float32(0)
+			if totalNumberOfTasks != 0 {
+				progress = float32(numberOfCompletedTasks) / float32(totalNumberOfTasks)
+			}
+			
+			t.AppendRow([]any{
+				i + 1, 
+				board.Name, 
+				totalNumberOfTasks,
+				fmt.Sprint("%", int(progress * 100)),
+				board.Dir,
+			})
+		}
+		t.AppendSeparator()
+		
+		t.Render()
 	},
 }
--- go.mod
+++ go.mod
@@ -2,17 +2,21 @@ module github.com/okira-e/gotasks
 
 go 1.23.0
 
-require github.com/spf13/cobra v1.8.1
+require (
+	github.com/gizak/termui/v3 v3.1.0
+	github.com/google/uuid v1.6.0
+	github.com/jedib0t/go-pretty/v6 v6.5.9
+	github.com/jinzhu/copier v0.4.0
+	github.com/sirupsen/logrus v1.9.3
+	github.com/spf13/cobra v1.8.1
+)
 
 require (
-	github.com/gizak/termui/v3 v3.1.0 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/jinzhu/copier v0.4.0 // indirect
-	github.com/mattn/go-runewidth v0.0.2 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
 	github.com/nsf/termbox-go v0.0.0-20190121233118-02980233997d // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 )
